fc4bcb4e4a94532a43eb8acc0e0a25a595e635d6
k2 <izmail.k2@gmail.com>
2023-12-17 21:14:16 +0300

    feat: in PopularJobsCard check image url

--- src/common/components/PopularJobsCard.tsx
+++ src/common/components/PopularJobsCard.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import {Image, Text, TouchableOpacity, View, StyleSheet} from "react-native";
 import {COLORS, FONT, SIZES} from "@/assets/style/globalStyle";
+import {checkImageUrl} from "@/common/utils/checkImageURL";
 
-
+const jobImageDefault = require('@/assets/img/job/job.jpg')
 type Props = {
   item: any,
 }
 export const PopularJobsCard = ({item}: Props) => {
+const imageLogo = checkImageUrl(item.employer_logo) ? item.employer_logo : jobImageDefault
+
   return (
     <TouchableOpacity onPress={() => {
     }} style={styles.container}>
       <TouchableOpacity>
-        <Image source={{uri: item.employer_logo}} resizeMode={'contain'} style={styles.logoImage}/>
+        <Image source={{uri: imageLogo}} resizeMode={'contain'} style={styles.logoImage}/>
       </TouchableOpacity>
       <Text style={styles.text} numberOfLines={1}>{item.employer_name}</Text>
       <View>
@@ -24,15 +27,14 @@ export const PopularJobsCard = ({item}: Props) => {
 
 const styles = StyleSheet.create({
   container: {
-    marginHorizontal: 10,
-    marginVertical: 20,
+    marginVertical: 10,
     justifyContent: 'flex-start',
     alignItems: "center",
-    width:150
+    width:140
   },
   logoImage: {
-    width: 70,
-    height: 70,
+    width: 60,
+    height: 60,
     borderRadius: 10
   },
   text: {
